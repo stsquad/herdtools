a34afe913979e9a646baf5ad55465a2ff387665b
John Wickerson <johnwickerson@cantab.net>
2016-02-22 21:35:23 +0000

    Collapsing multiple arguments of the same type.

--- herd/herd2alloy.ml
+++ herd/herd2alloy.ml
@@ -109,7 +109,6 @@ and alloy_of_exp args chan = function
   | Fun _ -> fprintf chan "Local functions not done yet"
   | _ -> Warn.fatal "explicitset/match/tag etc. in herd2alloy"
 
-
 and alloy_of_binding chan (x, e) = 
   match e with
   | Fun (_,_,_,_,_) ->
@@ -120,29 +119,10 @@ and alloy_of_binding chan (x, e) =
      else if x = "alloy_ignore_section_end" then
        in_ignore_section := false
      else if not !in_ignore_section then
-       fprintf chan "(y.%s) = %a\n\n" 
-               x 
-               (alloy_of_exp []) e
-
-and alloy_of_binding' chan (x, e) = 
-  match e with
-  | Fun (_,_,_,_,_) ->
-     fprintf chan "Functions not done yet"
-  | _ ->
-     if x = "alloy_ignore_section_begin" then
-       in_ignore_section := true
-     else if x = "alloy_ignore_section_end" then
-       in_ignore_section := false
-     else if not !in_ignore_section then
-       fprintf chan "fun %s[x : BasicExec, rf : E -> E, co : E -> E%s] : E -> E {\n  %a\n}\n\n" 
+       fprintf chan "fun %s[x : BasicExec, rf, co%s : E -> E] : E -> E {\n  %a\n}\n\n" 
                x
-	       (if !with_sc then ", s : E -> E" else "")
+	       (if !with_sc then ", s" else "")
                (alloy_of_exp []) e
-
-let fprintf_so x chan so = 
-  fprintf chan "%s" (match so with
-    | None -> x
-    | Some s -> s)
 
 let alloy_of_test = function
   | Acyclic -> "acyclic"
@@ -152,33 +132,24 @@ let alloy_of_test = function
 let provides : string list ref = ref []
 let requires : string list ref = ref []
 let seen_requires_clause : bool ref = ref false
-	   
-let find_derived_relations prog =
-  List.concat
-    (List.map
-       (function
-	 | Let (_,b) -> List.map fst b
-	 | _ -> [])
-       prog)
 					  
+
 let print_derived_relations chan = function
   | Let (_,bs) -> List.iter (alloy_of_binding chan) bs
   | _ -> ()
 
-let print_derived_relations' chan = function
-  | Let (_,bs) -> List.iter (alloy_of_binding' chan) bs
-  | _ -> ()
-
 let print_predicates chan = function
   | Test (_,_, test, exp, name, test_type) ->
-    let name = begin match name with 
-        | None -> Warn.user_error "You need to give each constraint a name!"
-        | Some name -> name 
- 	       end
-    in
-    fprintf chan "pred %s[x : BasicExec, rf : E -> E, co : E -> E%s] {\n  %s[%a]\n}\n\n" 
+     let name = begin
+	 match name with 
+	 | None ->
+	    Warn.user_error "You need to give each constraint a name!"
+         | Some name -> name 
+       end
+     in
+    fprintf chan "pred %s[x : BasicExec, rf, co%s : E -> E] {\n  %s[%a]\n}\n\n" 
 	    name
-	    (if !with_sc then ", s : E -> E" else "")
+	    (if !with_sc then ", s" else "")
 	    (alloy_of_test test)
 	    (alloy_of_exp []) exp;
     begin match test_type with
@@ -192,54 +163,24 @@ let print_predicates chan = function
     end
 
   | _ -> ()
-					  
-let alloy_of_ins chan = function
-  | Let (_,bs) -> List.iter (alloy_of_binding chan) bs
-  | Test (_,_, test, exp, name, test_type) ->
-    let name = begin match name with 
-        | None -> Warn.user_error "You need to give each constraint a name!\n"
-        | Some name -> name 
-    end in
-    (*fprintf chan "let %s X = %s [%a]" 
-      name
-      (alloy_of_test test)
-      (alloy_of_exp []) exp;*)
-    begin match test_type with
-      | Provides -> 
-        if (!seen_requires_clause) then
-          Warn.user_error "Provides-clause follows requires-clause!";
-        provides := name :: (!provides)
-      | Requires -> 
-        seen_requires_clause := true;
-        requires := name :: (!requires)
-    end
-  | UnShow _ | Show _ | ShowAs _ | Latex _ -> ()
-  | Include (_,file) ->
-    let file = String.capitalize file in
-    let file = String.sub file 0 (String.length file - 4) in
-    provides := (sprintf "%s.provides_clauses" file) :: (!provides);
-    requires := (sprintf "%s.requires_clauses" file) :: (!requires);
-    fprintf chan "open import %s" file
-  | ProcedureTest _ | Procedure _ | Call _ | Enum _ | Debug _
-  | Foreach _ | ForOrder _ | Rec _ ->
-      Warn.fatal "procedure/call/enum/debug/foreach in herd2alloy"
+					 
 
 let alloy_of_prog chan (with_sc_arg:bool) prog =
 
   with_sc := with_sc_arg;
   
-  List.iter (print_derived_relations' chan) prog;
+  List.iter (print_derived_relations chan) prog;
 
   List.iter (print_predicates chan) prog;
 
-  fprintf chan "pred consistent[x : BasicExec, rf : E -> E, co : E -> E] {\n      ";
+  fprintf chan "pred consistent[x : BasicExec, rf, co : E -> E] {\n      ";
   if !with_sc then
     fprintf chan "  some s : E -> E when ax_wfS[x,s] |\n      ";
   (*  fprintf chan "  some y : Extras | wf_Extras[x,y%s] &&\n    " (if !with_sc then ",s" else ""); *)
   fprintf_iter "\n    && " (fun chan s -> fprintf chan "%s[x,rf,co%s]" s (if !with_sc then ",s" else "")) chan (List.rev !provides);
   fprintf chan "\n}\n\n";
 
-  fprintf chan "pred not_faulty[x : BasicExec, rf : E -> E, co : E -> E] {\n      ";
-  fprintf_iter "\n    && " (fun chan s -> fprintf chan "%s[x,rf,co%s]" s (if !with_sc then ",none->none" else "")) chan (List.rev !requires);
+  fprintf chan "pred not_faulty[x : BasicExec, rf : E -> E] {\n      ";
+  fprintf_iter "\n    && " (fun chan s -> fprintf chan "%s[x,rf,none->none%s]" s (if !with_sc then ",none->none" else "")) chan (List.rev !requires);
   fprintf chan "\n}\n\n";
   
